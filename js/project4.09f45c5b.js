3dd98315f6c9d4f009a38a758f8cac08893fafe5
Serhii <65346189+zarka07@users.noreply.github.com>
2022-04-06 18:50:21 +0300

    project 3 worked

---
+++ js/project4.09f45c5b.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkvue3"]=self["webpackChunkvue3"]||[]).push([[116],{1603:function(e,n,r){r.r(n),r.d(n,{default:function(){return i}});var t=r(3396);const o=(0,t._)("div",null," project4 ",-1);function u(e,n,r,u,c,a){const p=(0,t.up)("Header"),s=(0,t.up)("Footer");return(0,t.wg)(),(0,t.iD)("div",null,[(0,t.Wm)(p),o,(0,t.Wm)(s)])}var c=r(7818),a=r(6996),p={name:"project4-component",components:{Header:c.Z,Footer:a.Z}},s=r(89);const d=(0,s.Z)(p,[["render",u]]);var i=d}}]);
+//# sourceMappingURL=project4.09f45c5b.js.map
